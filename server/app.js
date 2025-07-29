d921fa8eaf8bbc7579e50b841147a2cc66f7d46f
Zefi Kavvadia <zefi.kavvadia@di.huc.knaw.nl>
2025-07-29 17:54:24 +0200

    add mongodb connection and student and cohort models

--- server/app.js
+++ server/app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const cohorts = require("./cohorts.json")
-const students = require("./students.json")
-const cors = require('cors')
+
+const mongoose = require("mongoose");
+const cors = require("cors");
 const PORT = 5005;
 
-const cors = require("cors");
+const Student = require("./models/Student.model");
+const Cohort = require("./models/Cohort.model");
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(error => console.log("error connecting to database", error))
 
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-const cohorts = require("./cohorts.json")
-const students = require("./students.json")
+// const cohorts = require("./cohorts.json")
+// const students = require("./students.json")
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -37,12 +44,23 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+// app.get("/api/cohorts", (req, res, next) => {
+//   res.json(cohorts)
+// })
 app.get("/api/cohorts", (req, res, next) => {
-  res.json(cohorts)
+  Cohort.find({})
+  .then((cohorts) => {
+    res.json(cohorts)
+  })
+  .catch(error => console.log(error))
 })
 
 app.get("/api/students", (req, res, next) => {
-  res.json(students)
+  Student.find({})
+  .then((students) => {
+    res.json(students)
+  })
+  .catch(error => console.log(error))
 })
 
 // START SERVER
